3820250da5688759a28266d0dc8ebd67a276fae1
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-04-13 09:36:50 -0400

    Added Player swap on click and icons

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const App = {
     newRound: document.querySelector('[data-id="new-round-btn"]'),
     squares: document.querySelectorAll('[data-id="square"]'),
   },
+  state: {
+    currentPLayer: 1,
+  },
 
   init() {
     App.registerEventListeners();
@@ -29,6 +32,18 @@ const App = {
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
         console.log(`Square with id ${event.target.id} was clicked`);
+        console.log(`Current player is ${App.state.currentPLayer}`);
+        const currentPlayer = App.state.currentPLayer;
+        const icon = document.createElement("i");
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
+        App.state.currentPLayer = App.state.currentPLayer === 1 ? 2 : 1;
+        event.target.replaceChildren(icon);
+        // <i class="fa-solid fa-x yellow"></i>
+        // <i class="fa-solid fa-o turquoise"></i>
       });
     });
   },
