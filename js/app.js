26bbedee1d6c8e3f571567d035ab5d89571ca766
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-04-23 11:50:18 -0400

    added win conditions

--- js/app.js
+++ js/app.js
@@ -11,6 +11,34 @@ const App = {
     moves: [],
   },
 
+  getGameStatus(moves) {
+    const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId);
+    const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId);
+    const winningPatterns = [
+      
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let winner = null;
+    winningPatterns.forEach(pattern => {
+      //console.log({pattern, p1Moves, p2Moves})
+      const p1Wins = pattern.every( v => p1Moves.includes(v));
+      const p2Wins = pattern.every( v => p2Moves.includes(v));
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
+      winner // 1 | 2 | null
+    };
+  },
+
   init() {
     App.registerEventListeners();
   },
@@ -66,15 +94,15 @@ const App = {
         // <i class="fa-solid fa-o turquoise"></i>
 
         // Check for winner to tie game
-        const winningPatterns = [
-          [1, 2, 3],
-          [1, 5, 9],
-          [1, 4, 7],
-          [2, 5, 8],
-          [5, 5, 7],
-          [3, 6, 9],
-          [7, 8, 9],
-        ];
+        const game = App.getGameStatus(App.state.moves);
+        console.log(game);
+        if (game.status === 'complete'){
+          if (game.winner) {
+            alert(`Player ${game.winner} Wins!`)
+          } else {
+            alert("Tie!")
+          }
+        }
       });
     });
   },
